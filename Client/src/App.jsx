1a16212f1a66e7b7be6407277ca6858d335851f9
Carlos Urzua <urzua847@gmail.com>
2023-08-01 02:46:37 -0400

    creacion usuario

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,6 +4,8 @@ import EvaluationPage from "./pages/evaluation/evaluationPage.jsx";
 import RecordPage from "./pages/record/RecordPage";
 import NotificationPage from "./pages/notification/NotificationPage";
 import InventoryPage from "./pages/inventory/inventory.jsx";
+import UserPage from "./pages/user/UserPage";
+
 
 
 
@@ -16,7 +18,7 @@ function App(){
         <Route path='/inventory' element={<InventoryPage />} />
         <Route path='/notification' element={<NotificationPage />} />
         <Route path='/record' element={<RecordPage />} />
-        <Route path='/user' element={<h1> Usuario </h1>} />
+        <Route path='/user' element={<UserPage />} />
         <Route path='/analysis' element={<h1> Analisis </h1>} />
         <Route path='/supportAlert' element={<h1> Solicitud de equipo extra </h1>} />
       </Routes>
