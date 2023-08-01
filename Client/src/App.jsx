ad6ac934e01cfcb3befdae10837e495059dfc6a0
tottyib <tottyandbmx@gmail.com>
2023-07-31 23:53:33 -0400

    alerta

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,6 +4,7 @@ import EvaluationPage from "./pages/evaluation/evaluationPage.jsx";
 import RecordPage from "./pages/record/RecordPage";
 import NotificationPage from "./pages/notification/NotificationPage";
 import InventoryPage from "./pages/inventory/inventory.jsx";
+import SupportAlertPage from "./pages/support_alert/supportAlert.jsx";
 
 
 
@@ -18,7 +19,7 @@ function App(){
         <Route path='/record' element={<RecordPage />} />
         <Route path='/user' element={<h1> Usuario </h1>} />
         <Route path='/analysis' element={<h1> Analisis </h1>} />
-        <Route path='/supportAlert' element={<h1> Solicitud de equipo extra </h1>} />
+        <Route path='/supportAlert' element={<SupportAlertPage />} />
       </Routes>
     </BrowserRouter>  
   );
