39746a75fc1838cd13823789967b094c1bb1af2d
Nelson Venegas <nelsonvenegas1@gmail.com>
2023-07-31 22:18:47 -0400

    cambio nombre

--- Client/src/pages/inventory/inventory.jsx
+++ Client/src/pages/inventory/inventory.jsx
@@ -13,40 +13,34 @@ function InventoryPage() {
   return (
     <div className="flex h-[calc(100vh-100px)] items-center justify-center "> <div/> 
       <div className="bg-zinc-800 max-w-md p-10 rounded-md">
-        <h1 className="text-2xl text-center text-white font-bold mt-10">Registro</h1>
+        <h1 className="text-2xl text-center text-white font-bold mt-10">Add inventario</h1>
         <form onSubmit={onSubmit}>
           
           <p>
           <label>Pieza de equipo</label>
-          <input type="text" {... register('Nombre', {required: true})} 
+          <input type="text" {... register('equipment_name', {required: true})} 
             placeholder="nombre del equipo" className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-1 " 
           />
           </p>
           <br/>
 
           <p>Codigo</p>
-          <input type="number" {... register('phone', {required: true})} 
+          <input type="number" {... register('equipment_code', {required: true})} 
             placeholder="12345" className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" 
           />
-
-          <br/>
-          
           <p>Descripcion</p>
-          <input type="text" {... register('ubication', {required: true})} 
+          <input type="text" {... register('equipment_description', {required: true})} 
             placeholder="ingrese decripcion aqui" 
             className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" 
           />
-          
-          <br/>
-          
           <p>Disponible</p>
-          <input type="number"{... register('cantidad', {required: true})} 
+          <input type="number"{... register('quantity_available', {required: true})} 
             placeholder="indique cantidad a ingresar"
             className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" 
           />
 
           <p>Expiracion</p>
-          <input type="date"{... register('expira', {required: false})} 
+          <input type="date"{... register('expiration_date', {required: false})} 
             placeholder="indique fecha de expiracion si es que posee"
             className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" 
           />
